f5d7134144093f548aad059c648acffe1ae71c23
Huy <tudomb@gmal.com>
2025-03-11 16:04:00 +0700

    Xong tutorial

---
+++ WebBanSet/WebBanSet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebBanSet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WebBanSet/WebBanSet/Models/ProductDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebBanSet.Models
+{
+    public class ProductDBContext : DbContext
+    {
+        public ProductDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ProductImage> PI { get; set; }
+    }
+}
